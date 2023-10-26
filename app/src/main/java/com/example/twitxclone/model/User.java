96eb240674c4eb9497e184da2e8d2fb0efac61b6
estebanparrarodriguez <esteban.parrarodriguez@belmont.edu>
2023-10-26 07:06:02 -0500

    Added final keys to the User class

--- app/src/main/java/com/example/twitxclone/model/User.java
+++ app/src/main/java/com/example/twitxclone/model/User.java
@@ -3,6 +3,8 @@ package com.example.twitxclone.model;
 public class User {
     String username;
     String dob;
+    public static final String U_KEY = "user";
+    public static final String D_KEY = "birth";
 
     public String getUsername() {
         return username;
