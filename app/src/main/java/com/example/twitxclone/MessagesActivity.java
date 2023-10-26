2a17991dab34043275856fedd252aba1c35ebae8
estebanparrarodriguez <esteban.parrarodriguez@belmont.edu>
2023-10-25 19:36:08 -0500

    Added Writing to Firebase

--- app/src/main/java/com/example/twitxclone/MessagesActivity.java
+++ app/src/main/java/com/example/twitxclone/MessagesActivity.java
@@ -8,8 +8,11 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.twitxclone.model.Message;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -25,6 +28,8 @@ public class MessagesActivity extends AppCompatActivity {
     TextView userTextView;
     ListView listView;
 
+    FirebaseDatabase database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,14 @@ public class MessagesActivity extends AppCompatActivity {
         listView = (ListView) findViewById(R.id.listView);
         adapter = new MessageAdapter(this, messageList);
         listView.setAdapter(adapter);
+
+        // Get Intent and populate user and DOB
+
+
+        database = FirebaseDatabase.getInstance();
+
+        // Read messages form database
+
     }
 
     public void submitMessage(View view) {
@@ -45,7 +58,12 @@ public class MessagesActivity extends AppCompatActivity {
         newMessage.setAuthor(username);
         newMessage.setMessage(message);
         newMessage.setPublishedAt(new GregorianCalendar().getTimeInMillis());
-//        mDatabase.child(mDatabase.push().getKey()).setValue(newMessage);
+        DatabaseReference mDatabase =  database.getReference("messages");
+        String key = mDatabase.push().getKey();
+        if(key != null) {
+            mDatabase.child(key).setValue(newMessage);
+            Toast.makeText(this, key + " " + newMessage.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
         messageField.getText().clear();
     }
